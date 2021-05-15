3d0eac0fd4afae10833929c717f50ea66369b986
nibble0101 <mj.mawa@yahoo.com>
2021-05-15 22:11:31 +0300

    Add timezone config and emptied database

--- server.js
+++ server.js
@@ -6,34 +6,38 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const task = cron.schedule("* * * * *", () => {
-  fs.readFile("./data.json", (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    data = JSON.parse(data);
-    data.push(new Date().toISOString());
-    fs.writeFile("./data.json", JSON.stringify(data, null, 2), err => {
-      if(err){
+const task = cron.schedule(
+  "0 0 0 * * *",
+  () => {
+    fs.readFile("./data.json", (err, data) => {
+      if (err) {
         console.log(err);
         return;
       }
-      console.log("Written successfully to file");
-    } )
-  });
-});
+      data = JSON.parse(data);
+      data.push(new Date().toISOString());
+      fs.writeFile("./data.json", JSON.stringify(data, null, 2), (err) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log("Written successfully to file");
+      });
+    });
+  },
+  { scheduled: true, timezone: "Africa/Nairobi" }
+);
 
 task.start();
 
 app.get("/", (req, res) => {
   fs.readFile("./data.json", (err, data) => {
-    if(err){
+    if (err) {
       res.send(err);
-      return
+      return;
     }
     res.json(JSON.parse(data));
-  })
+  });
 });
 
 app.listen(PORT, () => console.log(`server listening on PORT ${PORT}`));
