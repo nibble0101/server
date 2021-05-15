9c1d78560dba701056bbc18a8a6cb22f2dc75667
nibble0101 <mj.mawa@yahoo.com>
2021-05-15 22:00:59 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const os = require("os");
+
+console.log(os.getPriority());
+
+for(const prop in os){
+
+}
+
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cron = require("node-cron");
+const fs = require("fs");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const task = cron.schedule("* * * * *", () => {
+  fs.readFile("./data.json", (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    data = JSON.parse(data);
+    data.push(new Date().toISOString());
+    fs.writeFile("./data.json", JSON.stringify(data, null, 2), err => {
+      if(err){
+        console.log(err);
+        return;
+      }
+      console.log("Written successfully to file");
+    } )
+  });
+});
+
+task.start();
+
+app.get("/", (req, res) => {
+  fs.readFile("./data.json", (err, data) => {
+    if(err){
+      res.send(err);
+      return
+    }
+    res.json(JSON.parse(data));
+  })
+});
+
+app.listen(PORT, () => console.log(`server listening on PORT ${PORT}`));
